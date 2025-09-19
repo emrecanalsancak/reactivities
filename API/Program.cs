0474f4689401e129b426dbb7f7d0a0a512c05f84
emre can alsancak <ec.alsancak@gmail.com>
2025-09-19 17:05:44 +0300

    Added SignalR

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using API.SignalR;
 using Application.Activities.Queries;
 using Application.Activities.Validators;
 using Application.Core;
@@ -28,6 +29,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddSignalR();
 
 builder.Services.AddMediatR(x =>
 {
@@ -70,6 +72,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<User>();
+app.MapHub<CommentHub>("/comments");
 
 
 using var scope = app.Services.CreateScope();
--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -29,6 +29,15 @@ type Profile = {
   imageUrl?: string;
 };
 
+type ChatComment = {
+  id: string;
+  createdAt: Date;
+  body: string;
+  userId: string;
+  displayName: string;
+  imageUrl?: string;
+};
+
 type User = {
   id: string;
   email: string;
